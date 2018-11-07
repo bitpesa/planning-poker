914cacc1395de399f59828ea176fd01c200a8dfe
Jerome Pratt <jerome@bitpesa.co>
2018-11-07 13:43:23 +0000

    Respond to slash commands asking for estimates
    This adds a class to send a message to the relevant slack hook url,
    which will allow users to estimate

--- app/controllers/poker_sessions_controller.rb
+++ app/controllers/poker_sessions_controller.rb
@@ -1,8 +1,10 @@
 class PokerSessionsController < ApplicationController
   def create
+    story_name = params[:text]
     poker_session = PokerSession.new
     if poker_session.save
-      render json: { session_id: poker_session.id }
+      PokerSlackMessage.new(story_name, poker_session.id).send
+      head 200
     else
       head 422
     end
