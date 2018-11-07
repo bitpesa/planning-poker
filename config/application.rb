cd78b286d8888735d7527b331ebdd2f9d796c90a
Jerome Pratt <jerome@bitpesa.co>
2018-11-07 13:53:32 +0000

    Load lib automatically

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,6 @@ module PlanningPoker
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.autoload_paths << "#{config.root}/lib"
   end
 end
