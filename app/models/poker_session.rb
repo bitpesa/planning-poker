849047be25200b2f17002297910e31e13ebbcef1
Jerome Pratt <jerome@bitpesa.co>
2018-11-12 17:28:42 +0000

    Take higher story point estimate when given the choice

--- app/models/poker_session.rb
+++ app/models/poker_session.rb
@@ -4,16 +4,35 @@ class PokerSession < ApplicationRecord
   STORY_POINTS = [1, 2, 3, 5, 8, 13, 20, 40, 100]
 
   def complete_session
-    average = estimates.pluck(:number).sum / estimates.count
-
-    differences = STORY_POINTS.map do |story_point|
-      [story_point, (average - story_point).abs]
-    end
-
-    closest_sprint_number = differences.min_by { |arr| arr[1] }[0]
-    self.result = closest_sprint_number
+    self.result = closest_story_number_to_average
     self.completed = true
     save
+  end
+
+  def closest_story_number_to_average
+    story_points_with_differences.inject([]) do |memo, array|
+      if memo.empty?
+        memo = array
+        break
+      end
+
+      if memo[1] > array[1]
+        memo = array
+      elsif memo[1] == array[1]
+        memo = [memo, array].max_by {|arr| arr[0]}
+      end
+      memo
+    end
+  end
+
+  def story_points_with_differences
+    STORY_POINTS.map do |story_point|
+      [story_point, (average_estimate - story_point).abs]
+    end
+  end
+
+  def average_estimate
+    estimates.pluck(:number).sum / estimates.count
   end
 
   def already_voted_text
